43d21dd3b7dc6bccc89925ad85894d63afafa505
DCJens <jens585@hotmail.com>
2016-10-13 15:08:37 +0200

    Aanpassing aan foreach

--- opdracht-looping-statements-foreach/opdracht-looping-statements-foreach-deel-2.php
+++ opdracht-looping-statements-foreach/opdracht-looping-statements-foreach-deel-2.php
@@ -7,15 +7,6 @@
 	
 ?>
 
-<?php
-$text=file_get_contents("text-file.txt");
-$textlowwer=strtolower($text);
-$count=count_chars($textlowwer);
-foreach (count_chars($textlowwer, 1) as $i => $val) {
-   echo "Er was $val keer  \"" , chr($i) , "\" in de tekst." . "<br>";
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,8 +15,8 @@ foreach (count_chars($textlowwer, 1) as $i => $val) {
 </head>
 <body>
 	<h2>Deel 2:</h2>
-	<?php foreach (count_chars(tekstlower, 1) as $i => $value): ?>
-		<li>Er was <?= $val ?> keer <?php chr($i) ?> in de tekst.</li>
+	<?php foreach (count_chars($tekstlower, 1) as $i => $value): ?>
+		<li>Er was <?= $value ?> keer " <?= chr($i) ?> " in de tekst.</li>
 	<?php endforeach ?>
 </body>
 </html>
